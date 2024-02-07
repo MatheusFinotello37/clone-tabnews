a48c4b8e0c514a7322851b27dee91797f40cb6b0
MatheusFinotello37 <134652781+MatheusFinotello37@users.noreply.github.com>
2024-02-07 21:16:57 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
+function home() {
+  return (
+    (<img src="coffee-shop.gif" alt="Imagem 1" width="150" height="100" />),
+    (
+      <img
+        src="statue-of-liberty.gif"
+        alt="Imagem 2"
+        width="150"
+        height="100"
+      />
+    ),
+    (<img src="save-money.gif" alt="Imagem 3" width="150" height="100" />)
+  );
+}
 
+export default home;
